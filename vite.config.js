a600911bd48da3aa9eca0d4028a9e5cf3f203495
Speakeasy <speakeasy_daddy@outlook.com>
2026-06-02 16:11:29 -0400

    Modifications

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/compare-questionnaire/'
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
