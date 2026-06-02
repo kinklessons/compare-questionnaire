02b77d4dab3922f78fb543ca152eb75531d1acf6
Speakeasy <speakeasy_daddy@outlook.com>
2026-06-02 16:17:11 -0400

    Modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-#import { Card, CardContent } from "@/components/ui/card";
-#import { Button } from "@/components/ui/button";
-#import { Input } from "@/components/ui/input";
 
 // Import your question list
 
